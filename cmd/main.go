c5cdf52d6001677e7c53c851301ff1884c609a76
b9p31049 <b9p31049@shonan.bunkyo.ac.jp>
2021-12-26 17:15:06 +0900

    fix WORKSPACE, BUILD.bazel

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bazel/uuid"
+	"log"
+)
+
+func main() {
+	id, err := uuid.Generate()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(id)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module bazel
+
+go 1.17
+
+require github.com/google/uuid v1.3.0
